6b0ced2d128674f359e5e804fb98cc3b8fe8b474
Alinda Byamukama <alindabyamukama@gmail.com>
2025-07-01 14:19:25 +0300

    add update to lines 4, 7, 12

--- my-app/app/(tabs)/index.tsx
+++ my-app/app/(tabs)/index.tsx
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const index = () => {
+const app = () => {
   return (
     <View>
-      <Text>index</Text>
+      <Text>App</Text>
     </View>
   )
 }
 
-export default index
+export default app
