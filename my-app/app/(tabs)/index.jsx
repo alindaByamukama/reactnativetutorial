2b6c502e6d2f7106d31f8efd0c318ef1d60695c7
Alinda Byamukama <alindabyamukama@gmail.com>
2025-07-01 15:22:07 +0300

    add update to text styles, add background image and styles

---
+++ my-app/app/(tabs)/index.jsx
@@ -0,0 +1,37 @@
+import { View, Text, StyleSheet, ImageBackground } from 'react-native'
+import React from 'react'
+import icedCoffeeImg from '../../assets/images/iced-coffee.png'
+
+const app = () => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={icedCoffeeImg} resizeMode='cover' style={styles.image}>
+        <Text style={styles.text}>Coffee Shop</Text>
+      </ImageBackground>
+    </View>
+  )
+}
+
+export default app
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 42,
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginTop: 20,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Semi-transparent background for better readability
+  },
+})
