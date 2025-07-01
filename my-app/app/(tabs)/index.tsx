dbfea6aa1c96e8199960c937ab0eb4278fe4e70a
Alinda Byamukama <alindabyamukama@gmail.com>
2025-07-01 15:07:55 +0300

    add styles to text, container

--- my-app/app/(tabs)/index.tsx
+++ my-app/app/(tabs)/index.tsx
@@ -1,12 +1,26 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 const app = () => {
   return (
-    <View>
-      <Text>Coffee Shop</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Coffee Shop</Text>
     </View>
   )
 }
 
 export default app
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  text: {
+    fontSize: 42,
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginTop: 20,
+  },
+})
